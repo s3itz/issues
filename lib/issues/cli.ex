8a4647f95bb8b497739c84f5cf73e6687087a930
Daniel Seitz <daniel.e.seitz@gmail.com>
2016-03-01 04:32:18 -0600

    Add pretty printing

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -4,6 +4,8 @@ defmodule Issues.CLI do
   that end up generating a table of the last _n_ issues in a GitHub project.
   """
 
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
+
   @default_count 4
 
   def run(argv) do
@@ -41,10 +43,13 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GitHubIssues.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_maps
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}) do
